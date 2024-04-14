8ba0f55fa327f0e2d2ec9c1a2c5b15a7ef8c339c
Anonymous <124679005+PivoEnjoyer62@users.noreply.github.com>
2024-04-14 16:21:43 +0300

    second step
    over cool

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
 
@@ -35,20 +36,27 @@ class LoginForm(FlaskForm):
 
 
 class UpdateAccountForm(FlaskForm):
-    username = StringField("Ім'я користувача",
+    username = StringField('Нікнейм',
                            validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Емейл',
                         validators=[DataRequired(), Email()])
+    picture = FileField('Поставити аватар', validators=[FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Оновити')
 
     def validate_username(self, username):
         if username.data != current_user.username:
             user = User.query.filter_by(username=username.data).first()
             if user:
-                raise ValidationError("Це ім'я користувача вже зайняте. Виберіть будь ласка інше.")
+                raise ValidationError("Це ім'я вже зайняте. Виберіть інше.")
 
     def validate_email(self, email):
         if email.data != current_user.email:
             user = User.query.filter_by(email=email.data).first()
             if user:
-                raise ValidationError("Цей емейл вже присутній. Виберіть будь ласка інший.")
+                raise ValidationError('Цей емейл вже зайнятий. Будь ласка виберіть інший.')
+
+
+class PostForm(FlaskForm):
+    title = StringField('Заголовок', validators=[DataRequired()])
+    content = TextAreaField('Контент', validators=[DataRequired()])
+    submit = SubmitField('Пост')
